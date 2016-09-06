585d9afeda1375ee9276a9ba649c4bb10138a4da
stw <brett.berry@stw-technic.com>
2016-09-06 11:23:13 +0200

    separate controller from module definition

---
+++ plugins/myplugin/controllers/backChannel.js
@@ -0,0 +1,6 @@
+angular.module('myapp.backchannel').controller('backChannelCtrl', ['$scope', function ($scope) {
+  'use strict';
+
+  $scope.hello = 'Hello world!';
+
+}]);
--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,5 +1,5 @@
 //Main module name must be defined in ngModules of the plugin manifest
-angular.module('myapp.helloworld', []).config(['c8yNavigatorProvider', 'c8yViewsProvider',
+angular.module('myapp.backchannel', []).config(['c8yNavigatorProvider', 'c8yViewsProvider',
 function (c8yNavigatorProvider, c8yViewsProvider) {
   'use strict';
 
@@ -13,14 +13,7 @@ function (c8yNavigatorProvider, c8yViewsProvider) {
   c8yViewsProvider.when('/hello', {
     // Please use this string placeholder where you want to refer you plugin path.
     templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-    controller: 'mh_MainCtrl'
+    controller: 'backChannelCtrl'
   });
-
-}]);
-
-angular.module('myapp.helloworld').controller('mh_MainCtrl', ['$scope', function ($scope) {
-  'use strict';
-
-  $scope.hello = 'Hello world!';
 
 }]);
