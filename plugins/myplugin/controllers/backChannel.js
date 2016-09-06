eae302ecfe3731f82c12fb77ced24550c77dbfc9
stw <brett.berry@stw-technic.com>
2016-09-06 14:37:06 +0200

    add list command test and button

--- plugins/myplugin/controllers/backChannel.js
+++ plugins/myplugin/controllers/backChannel.js
@@ -1,6 +1,13 @@
-angular.module('myapp.backchannel').controller('backChannelCtrl', ['$scope', function ($scope) {
+angular.module('myapp.backchannel').controller('backChannelCtrl', ['$scope', 'c8yDevices', function ($scope, c8yDevices) {
   'use strict';
 
-  $scope.hello = 'Hello world!';
+   $scope.hello = 'Hello world!';
+   $scope.sendCmd = function(){
+      console.log('got send cmd');
+   }
+
+   c8yDevices.list().then(function(devs){
+      console.log('device list: ', devs);
+   });
 
 }]);
