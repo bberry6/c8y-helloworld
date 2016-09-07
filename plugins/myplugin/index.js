2a834c77b922c0f8c9175641e4c0e83f3eccd8d7
stw <brett.berry@stw-technic.com>
2016-09-07 13:02:25 +0200

    ...

--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,7 +1,11 @@
 //Main module name must be defined in ngModules of the plugin manifest
-angular.module('myapp.backchannel', []).config(['c8yViewsProvider',
-function (c8yViewsProvider) {
+angular.module('myapp.backchannel', []).config(['c8yViewsProvider', 'c8yNavigator',
+function (c8yViewsProvider, c8yNavigator) {
   'use strict';
+
+
+  var navparts = c8yNavigator.getNavigator();
+  console.log('navparts: ', navparts);
 
   c8yViewsProvider.when('/device/:deviceId', {
         name: 'BackChannel',
