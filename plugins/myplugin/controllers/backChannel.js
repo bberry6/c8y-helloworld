9134df44e4c8d901e1e1d8b494ebf42b8794b5df
stw <brett.berry@stw-technic.com>
2016-09-07 10:41:54 +0200

    test  params

--- plugins/myplugin/controllers/backChannel.js
+++ plugins/myplugin/controllers/backChannel.js
@@ -1,13 +1,36 @@
-angular.module('myapp.backchannel').controller('backChannelCtrl', ['$scope', 'c8yDevices', function ($scope, c8yDevices) {
+var dependencies = ['$scope', '$routeParams', 'c8yDevices'];
+var backChannelController = function($scope, $routeParams, c8yDevices) {
   'use strict';
 
    $scope.hello = 'Hello world!';
    $scope.sendCmd = function(){
-      console.log('got send cmd');
+      console.log('device id: ', $routeParams.deviceId);
+      /*
+      var operation = {
+         deviceId: $routeParams.deviceId,
+         description: 'BYB Test Operation',
+         c8y_command: {
+            param1: 'paramValue'
+         }
+      };
+      c8yDeviceControl.createWithNotifications(operation).then(function (operationPromises) {
+         operationPromises.created.then(_.partial(c8yAlert.success, 'Operation ' + operation.description + ' has been created!'));
+         operationPromises.completed.then(function (operationResult) {
+         if (operationResult.status === c8yDeviceControl.status.SUCCESSFUL) {
+            handleSuccess();
+         } else if (operationResult.status === c8yDeviceControl.status.FAILED) {
+            handleFailure();
+         }
+         });
+      });
+      */
    }
 
    c8yDevices.list().then(function(devs){
       console.log('device list: ', devs);
    });
 
-}]);
+};
+
+backChannelController.$inject = dependencies;
+angular.module('myapp.backchannel').controller('backChannelCtrl', backChannelController);
