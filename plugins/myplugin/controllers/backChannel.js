adddffc066c9f641b80ee6aded2c1c17381a6cd2
stw <brett.berry@stw-technic.com>
2016-09-08 10:24:58 +0200

    update

--- plugins/myplugin/controllers/backChannel.js
+++ plugins/myplugin/controllers/backChannel.js
@@ -1,17 +1,17 @@
-var dependencies = ['$scope', 'c8yDevices'];
-var backChannelController = function($scope, c8yDevices) {
+var dependencies = ['$scope', '$routeParams', 'c8yDevices'];
+var backChannelController = function($scope, $routeParams, c8yDevices) {
   'use strict';
 
    $scope.hello = 'Hello cruel world!';
    $scope.sendCmd = function(){
-      /*
+
       console.log('device id: ', $routeParams.deviceId);
       if($routeParams.deviceId){
          c8yDevices.detail($routeParams.deviceId).then(function (res) {
             console.log('device  data: ', res);
          });
       }
-      */
+
       /*
       var operation = {
          deviceId: $routeParams.deviceId,
