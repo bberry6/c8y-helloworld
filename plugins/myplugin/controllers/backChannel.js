0a06151e6184a5b692005897b22aed6f7d401c17
stw <brett.berry@stw-technic.com>
2016-09-09 11:05:32 +0200

    foo

---
+++ plugins/myplugin/controllers/backChannel.js
@@ -0,0 +1,42 @@
+var dependencies = ['$scope', '$routeParams', 'c8yDevices'];
+var backChannelController = function($scope, $routeParams, c8yDevices) {
+  'use strict';
+
+   $scope.hello = 'Hello cruel world!';
+   $scope.sendCmd = function(){
+
+      console.log('device id: ', $routeParams.deviceId);
+      if($routeParams.deviceId){
+         c8yDevices.detail($routeParams.deviceId).then(function (res) {
+            console.log('device  data: ', res);
+         });
+      }
+
+      /*
+      var operation = {
+         deviceId: $routeParams.deviceId,
+         description: 'BYB Test Operation',
+         c8y_command: {
+            param1: 'paramValue'
+         }
+      };
+      c8yDeviceControl.createWithNotifications(operation).then(function (operationPromises) {
+         operationPromises.created.then(_.partial(c8yAlert.success, 'Operation ' + operation.description + ' has been created!'));
+         operationPromises.completed.then(function (operationResult) {
+         if (operationResult.status === c8yDeviceControl.status.SUCCESSFUL) {
+            handleSuccess();
+         } else if (operationResult.status === c8yDeviceControl.status.FAILED) {
+            handleFailure();
+         }
+         });
+      });
+      */
+   }
+
+   c8yDevices.list().then(function(devs){
+      console.log('device list: ', devs);
+   });
+
+};
+
+angular.module('myapp.backchannel').controller('backChannelCtrl', dependencies.concat(backChannelController));
--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,34 +1,31 @@
-(function(){
+//Main module name must be defined in ngModules of the plugin manifest
+var deps = ['c8yNavigatorProvider', 'c8yViewsProvider'];
+var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider) {
   'use strict';
 
-   //Main module name must be defined in ngModules of the plugin manifest
-   var deps = ['c8yNavigatorProvider', 'c8yViewsProvider'];
-   var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider) {
+  c8yNavigatorProvider.addNavigation({
+    name: 'New plugin',
+    icon: 'cube',
+    priority: 100000,
+    path: 'hello'
+  });
 
-     c8yNavigatorProvider.addNavigation({
-       name: 'See Hello',
-       icon: 'cube',
-       priority: 100000,
-       path: 'hello'
-     });
+  c8yViewsProvider.when('/hello', {
+    // Please use this string placeholder where you want to refer you plugin path.
+    templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+    controller: 'backChannelCtrl'
+  });
 
-     c8yViewsProvider.when('/hello', {
-       // Please use this string placeholder where you want to refer you plugin path.
-       templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-       controller: 'helloCtrl'
-     });
-
-   }
-   angular.module('myapp.helloworld', []).config(deps.concat(moduleFn));
+}
+angular.module('myapp.backchannel', []).config(deps.concat(moduleFn));
 
 
-   /*
-   c8yViewsProvider.when('/device/:deviceId', {
-      name: 'BackChannel',
-      icon: 'envelope-o',
-      priority: 1000,
-      templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-      controller: 'backChannelCtrl'
-   });
-   */
-})(angular);
+/*
+c8yViewsProvider.when('/device/:deviceId', {
+   name: 'BackChannel',
+   icon: 'envelope-o',
+   priority: 1000,
+   templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+   controller: 'backChannelCtrl'
+});
+*/
