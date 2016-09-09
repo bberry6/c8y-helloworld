3accbba11c834616035e31d7959e5cb40cf66057
stw <brett.berry@stw-technic.com>
2016-09-09 09:34:37 +0200

    ...

--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,22 +1,29 @@
 //Main module name must be defined in ngModules of the plugin manifest
-angular.module('myapp.backchannel', []).config(['c8yNavigatorProvider', 'c8yViewsProvider',
-function (c8yNavigatorProvider, c8yViewsProvider) {
-  'use strict';
+var deps = ['c8yNavigatorProvider', 'c8yViewsProvider', 'c8yDeviceGroup'];
+function moduleFn(c8yNavigatorProvider, c8yViewsProvider, c8yDeviceGroup) {
+   'use strict';
 
-  c8yNavigatorProvider.addNavigation({
-    name: 'New plugin',
-    icon: 'cube',
-    priority: 100000,
-    path: 'hello'
-  });
+   c8yDeviceGroup.list().then(function(r){
+      console.log('group list: ', r);
+   });
 
-  c8yViewsProvider.when('/hello', {
-    // Please use this string placeholder where you want to refer you plugin path.
-    templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-    controller: 'backChannelCtrl'
-  });
+   c8yNavigatorProvider.addNavigation({
+      name: 'New plugin',
+      icon: 'cube',
+      priority: 100000,
+      path: 'hello'
+   });
 
-}]);
+   c8yViewsProvider.when('/hello', {
+      // Please use this string placeholder where you want to refer you plugin path.
+      templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+      controller: 'backChannelCtrl'
+   });
+
+}
+
+
+angular.module('myapp.backchannel', []).config(deps.concat(moduleFn));
 
 
 /*
