7b48d07f78d5691a95b9e70373f66f1c2d7b7d32
stw <brett.berry@stw-technic.com>
2016-09-09 10:56:13 +0200

    ...

--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,31 +1,34 @@
-//Main module name must be defined in ngModules of the plugin manifest
-var deps = ['c8yNavigatorProvider', 'c8yViewsProvider'];
-var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider) {
+(function(){
   'use strict';
 
-  c8yNavigatorProvider.addNavigation({
-    name: 'See Hello',
-    icon: 'cube',
-    priority: 100000,
-    path: 'hello'
-  });
+   //Main module name must be defined in ngModules of the plugin manifest
+   var deps = ['c8yNavigatorProvider', 'c8yViewsProvider'];
+   var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider) {
 
-  c8yViewsProvider.when('/hello', {
-    // Please use this string placeholder where you want to refer you plugin path.
-    templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-    controller: 'helloCtrl'
-  });
+     c8yNavigatorProvider.addNavigation({
+       name: 'See Hello',
+       icon: 'cube',
+       priority: 100000,
+       path: 'hello'
+     });
 
-}
-angular.module('myapp.helloworld', []).config(deps.concat(moduleFn));
+     c8yViewsProvider.when('/hello', {
+       // Please use this string placeholder where you want to refer you plugin path.
+       templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+       controller: 'helloCtrl'
+     });
+
+   }
+   angular.module('myapp.helloworld', []).config(deps.concat(moduleFn));
 
 
-/*
-c8yViewsProvider.when('/device/:deviceId', {
-   name: 'BackChannel',
-   icon: 'envelope-o',
-   priority: 1000,
-   templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-   controller: 'backChannelCtrl'
-});
-*/
+   /*
+   c8yViewsProvider.when('/device/:deviceId', {
+      name: 'BackChannel',
+      icon: 'envelope-o',
+      priority: 1000,
+      templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+      controller: 'backChannelCtrl'
+   });
+   */
+})(angular);
