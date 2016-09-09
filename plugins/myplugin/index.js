5de6712b1143943045e91f9b443942d701e69223
stw <brett.berry@stw-technic.com>
2016-09-09 11:08:24 +0200

    foo

--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,6 +1,6 @@
 //Main module name must be defined in ngModules of the plugin manifest
-var deps = ['c8yNavigatorProvider', 'c8yViewsProvider'];
-var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider) {
+var deps = ['c8yNavigatorProvider', 'c8yViewsProvider', 'c8yDeviceGroup'];
+var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider, c8yDeviceGroup) {
   'use strict';
 
   c8yNavigatorProvider.addNavigation({
