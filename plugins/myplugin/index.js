189a2b8979399cb31e060b086c7f921a7d211f1e
stw <brett.berry@stw-technic.com>
2016-09-09 11:12:21 +0200

    foo

--- plugins/myplugin/index.js
+++ plugins/myplugin/index.js
@@ -1,20 +1,24 @@
 //Main module name must be defined in ngModules of the plugin manifest
 var deps = ['c8yNavigatorProvider', 'c8yViewsProvider', 'c8yDeviceGroup'];
 var moduleFn = function(c8yNavigatorProvider, c8yViewsProvider, c8yDeviceGroup) {
-  'use strict';
+   'use strict';
 
-  c8yNavigatorProvider.addNavigation({
-    name: 'New plugin',
-    icon: 'cube',
-    priority: 100000,
-    path: 'hello'
-  });
+   c8yNavigatorProvider.addNavigation({
+      name: 'New plugin',
+      icon: 'cube',
+      priority: 100000,
+      path: 'hello'
+   });
 
-  c8yViewsProvider.when('/hello', {
-    // Please use this string placeholder where you want to refer you plugin path.
-    templateUrl: ':::PLUGIN_PATH:::/views/index.html',
-    controller: 'backChannelCtrl'
-  });
+   c8yViewsProvider.when('/hello', {
+      // Please use this string placeholder where you want to refer you plugin path.
+      templateUrl: ':::PLUGIN_PATH:::/views/index.html',
+      controller: 'backChannelCtrl'
+   });
+
+   c8yDeviceGroup.list().then(function(r){
+      console.log('device groups: ', r);
+   });
 
 }
 angular.module('myapp.backchannel', []).config(deps.concat(moduleFn));
