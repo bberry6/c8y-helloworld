a7180e81eb5ed575c0c102a4696f464182b18099
stw <brett.berry@stw-technic.com>
2016-09-19 14:41:36 +0200

    ...

--- plugins/myplugin/controllers/device.js
+++ plugins/myplugin/controllers/device.js
@@ -1,8 +1,12 @@
 (function(){
    'use strict';
-   var deps = ['$scope', '$routeParams','c8yDeviceControl'];
-   var controllerFn = function($scope, $routeParams, c8yDeviceControl) {
+   var deps = ['$scope', '$routeParams','c8yDeviceControl', 'c8yDevices'];
+   var controllerFn = function($scope, $routeParams, c8yDeviceControl, c8yDevices) {
+
       $scope.devId = $routeParams.deviceId;
+      c8yDevices.detail($scope.devId).then(function(dev){
+         $scope.devName = dev.data.name;
+      });
    };
 
    angular.module('myapp.backchannel').controller('deviceCtrl', deps.concat(controllerFn));
